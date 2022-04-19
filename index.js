ef7c09a947b6571c3f1f8c6eaebb6301230db7a3
MacUer <macuer@MacUers-MacBook-Pro.local>
2022-04-18 21:16:58 -0400

    some message

---
+++ index.js
@@ -0,0 +1,38 @@
+
+//for loop
+const myArray = [];
+
+for(let i = 1; i < 10; i++) {
+    myArray.push(i);
+}
+
+//while loop
+
+const myArray = [];
+let i = 10;
+
+while(i >= 0){
+    myArray.push(i);
+    i--;
+}
+
+console.log(myArray)
+
+
+//odd numbers
+const myArray = [];
+
+for (let i = 11; i < 21; i += 2){
+myArray.push(i);
+}
+
+console.log(myArray)
+
+//backwards
+
+const myArray = [];
+for (let i = 15; i > 0; i -=2){
+    myArray.push(i);
+}
+
+console.log(myArray)
